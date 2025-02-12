5a2b2a628c03e07fe9ed57242e498ed2b846373e
Nayeong Koo <69792693+ooNa0@users.noreply.github.com>
2025-02-12 19:01:22 +0900

    Update 1.py

--- week4/1.py
+++ week4/1.py
@@ -3,11 +3,11 @@ import math
 def solution(n):
     answer = 0
     for i in range(2, n + 1):
-        tmp = 0
-        for k in range(2, i):
-            tmp += 1
+        flag = False
+        for k in range(2, int(math.sqrt(i)+1)):
             if(i % k == 0):
+                flag = True
                 break
-        if(tmp == i-2):
+        if(flag == False):
             answer += 1
     return answer
